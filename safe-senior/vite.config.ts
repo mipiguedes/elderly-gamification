8e44dc8f9f1a5f55855bad0fb7c499dc91eb613e
Milena Guedes <milena@hibrido.com.br>
2022-06-20 11:48:01 -0300

    deploy

--- safe-senior/vite.config.ts
+++ safe-senior/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/elderly-gamification/',
   plugins: [react()],
   server: {
     host: true
